2fe379333b3aea097d42742ebe5ab28fbf88349c
Mingyu Choi <cmg5691@g.cnu.ac.kr>
2023-12-09 07:57:04 +0900

    Update: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,9 +9,14 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <link
             rel="apple-touch-icon"
-            sizes="192x192"
-            href="/apple-touch-icon.png"
+            sizes="180x180"
+            href="/apple-icon-180x180.png"
           ></link>
+          <link rel="shortcut icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+          <link rel="manifest" href="/manifest.json">
+          <meta name="msapplication-TileColor" content="#ffffff">
+          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+          <meta name="theme-color" content="#ffffff"></meta>
           <link
             rel="alternate"
             type="application/rss+xml"
